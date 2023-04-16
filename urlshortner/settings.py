719ec4bdaa25b32610fc56e9d61abe37da7eebda
manthan-jsharma <manthan.jsharma@gmail.com>
2023-04-16 17:58:41 +0530

    added deploy changes

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -128,5 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://visionary-tartufo-a1bf9d.netlify.app/",
 ]
+
