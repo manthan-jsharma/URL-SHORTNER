d1d2606845a687d6c6b18b31a5a6651de494187c
manthan-jsharma <manthan.jsharma@gmail.com>
2023-12-28 23:10:54 +0530

    added: ALLOWED_HOSTS

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-eh(tut5&mu=a15=u*6f=1yvd@_*2b+mgw2j-5oe@2zu)!u8$87
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
